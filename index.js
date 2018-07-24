8eb668a7e99a202a8e88b430a66628a58caa84df
Hanna Strezh <hannastrezh@gmail.com>
2018-07-24 18:12:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object
 }// it is destructive because it adds a state //of the object
 function updateObjectWithKeyAndValue(object, key, value){
-  var object2 = Object.assign({prop2:value},object);
+  var object2 = Object.assign({pro:value},object);
   return object2;
   //need {}, as an argument to assign //function
 }
